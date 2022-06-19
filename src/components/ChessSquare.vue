64d36671c254eedaa41267de240309071bc976af
Simon Boeuf <simon.boeuf@gmail.com>
2022-06-19 23:56:56 +0200

    empty components

--- src/components/ChessSquare.vue
+++ src/components/ChessSquare.vue
@@ -1,72 +1,7 @@
-<script setup lang="ts">
-import { defineProps, defineEmits, computed } from "vue";
-import type { Position } from "@/types";
-import clsx from "clsx";
-
-const props = defineProps<{
-  position: Position;
-  selected: boolean;
-  isCandidate: boolean;
-  isEmpty: boolean;
-}>();
-
-const emit = defineEmits<{
-  (e: "select"): void;
-}>();
-
-const handleClik = () => {
-  emit("select");
-};
-
-const color = computed<string>(() => {
-  return (props.position.x + props.position.y) % 2 ? "black" : "white";
-});
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div @click="handleClik" :class="clsx(color, { selected }, 'square')">
-    <div
-      :class="clsx('candidate', { 'is-empty': props.isEmpty })"
-      v-if="isCandidate"
-    ></div>
-    <slot></slot>
-  </div>
+  <div></div>
 </template>
 
-<style lang="scss">
-.square {
-  height: 72px;
-  width: 72px;
-  cursor: pointer;
-  position: relative;
-  &.selected {
-    background: #646f3f;
-  }
-}
-.black {
-  background: #b58863;
-}
-.white {
-  background: #f0d9b5;
-}
-
-.candidate {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: radial-gradient(
-    transparent 0%,
-    transparent 79%,
-    rgba(20, 85, 0, 0.3) 80%
-  );
-  &.is-empty {
-    background: radial-gradient(
-      rgba(20, 85, 30, 0.5) 19%,
-      rgba(0, 0, 0, 0) 20%
-    );
-    pointer-events: auto;
-  }
-}
-</style>
+<style lang="scss"></style>
--- src/components/GameOver.vue
+++ src/components/GameOver.vue
@@ -1,27 +1,7 @@
-<script setup lang="ts">
-import { computed } from "vue";
-import { Player } from "@/types";
-const props = defineProps<{
-  winner: Player | null;
-}>();
-
-const text = computed(() => {
-  if (props.winner === null) {
-    return "The game is a draw";
-  }
-  const playerName = props.winner === Player.Black ? "Black" : "White";
-  return `${playerName} won !`;
-});
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div class="game-over">{{ text }}</div>
+  <div></div>
 </template>
 
-<style>
-.game-over {
-  color: black;
-  text-align: center;
-  font-size: 48px;
-}
-</style>
+<style></style>
